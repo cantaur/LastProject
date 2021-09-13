ee4564acaa76010bbc0354cbf4cfa5bc943318b1
kaeun <attackmood@gmail.com>
2021-09-14 01:36:19 +0900

    member쪽 TestController에 구현. project, milestone 쪽 주석처리(enum 이슈)

--- src/main/java/com/project/pium/service/MemberService.java
+++ src/main/java/com/project/pium/service/MemberService.java
@@ -4,5 +4,10 @@ import com.project.pium.domain.MemberDTO;
 import java.util.List;
 
 public interface MemberService {
-    List<MemberDTO> list();
+    List<MemberDTO> selectAll();
+    List<MemberDTO> selectAllByEmail(String mEmail);
+    List<MemberDTO> selectAllByMseq(long mSEQ);
+
+    void insertUser(MemberDTO memberDTO);
+    void deleteUser(long mSEQ);
 }
--- src/main/java/com/project/pium/service/ProjectServiceImpl.java
+++ src/main/java/com/project/pium/service/ProjectServiceImpl.java
@@ -1,32 +1,32 @@
-package com.project.pium.service;
-
-import com.project.pium.domain.ProjectDTO;
-import com.project.pium.mapper.ProjectMapper;
-import lombok.AllArgsConstructor;
-import lombok.extern.java.Log;
-import org.springframework.stereotype.Service;
-
-import java.util.List;
-
-@Log
-@AllArgsConstructor
-@Service
-public class ProjectServiceImpl implements ProjectService {
-
-    private ProjectMapper projectMapper;
-
-    @Override
-    public List<ProjectDTO> listS() {
-        return projectMapper.list();
-    }
-
-    @Override
-    public Long selectByMemberSeqS(String member_email) {
-        return projectMapper.selectByMemberSeq(member_email);
-    }
-
-    @Override
-    public void insertS(ProjectDTO projectDTO) {
-        projectMapper.insert(projectDTO);
-    }
-}
+//package com.project.pium.service;
+//
+//import com.project.pium.domain.ProjectDTO;
+//import com.project.pium.mapper.ProjectMapper;
+//import lombok.AllArgsConstructor;
+//import lombok.extern.java.Log;
+//import org.springframework.stereotype.Service;
+//
+//import java.util.List;
+//
+//@Log
+//@AllArgsConstructor
+//@Service
+//public class ProjectServiceImpl implements ProjectService {
+//
+//    private ProjectMapper projectMapper;
+//
+//    @Override
+//    public List<ProjectDTO> listS() {
+//        return projectMapper.list();
+//    }
+//
+//    @Override
+//    public Long selectByMemberSeqS(String member_email) {
+//        return projectMapper.selectByMemberSeq(member_email);
+//    }
+//
+//    @Override
+//    public void insertS(ProjectDTO projectDTO) {
+//        projectMapper.insert(projectDTO);
+//    }
+//}
