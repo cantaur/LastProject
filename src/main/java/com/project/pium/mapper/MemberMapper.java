13d9a1bbd8ffb38a085b38b84a9e4ae364aafc27
soo <dlrkwjstk77@gmail.com>
2021-09-13 21:54:37 +0900

    2021/09/03 김현수 Member.xml

--- src/main/java/com/project/pium/mapper/MemberMapper.java
+++ src/main/java/com/project/pium/mapper/MemberMapper.java
@@ -9,6 +9,9 @@ import java.util.List;
 @Repository
 @Mapper
 public interface MemberMapper {
-    List<MemberDTO> list();
-
+    List<MemberDTO> selectAll();
+    long selectByMseq(String member_email);
+    String selectByMemail(long member_seq);
+    String selectByMpw(long member_seq);
+    String selectByMplatform(long member_seq);
 }
