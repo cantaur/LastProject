00c69fd6b028835adfd647ea01cdaa25d17bb6dd
JeongGu <kjg3595@naver.com>
2021-09-14 19:03:37 +0900

    DTO클래스에 timestamp import, milestone 테스트(가은)

--- src/main/java/com/project/pium/controller/JGController.java
+++ src/main/java/com/project/pium/controller/JGController.java
@@ -4,10 +4,7 @@ import com.project.pium.domain.ProjectDTO;
 import com.project.pium.service.ProjectService;
 import lombok.AllArgsConstructor;
 import lombok.extern.java.Log;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -25,15 +22,66 @@ public class JGController {
         List<ProjectDTO> list = projectService.listS();
         return list;
     }
-
-    //http://127.0.0.1:8000/rest_jg/searchProject
-
-    @GetMapping("searchMemberSeq")
-    public Long selectByMemberSeq(String member_email){
+    //http://127.0.0.1:8000/rest_jg/searchProject 호출 성공
+    @GetMapping("searchMemberSeq/{member_email}")
+    public Long selectByMemberSeq(@PathVariable("member_email") String member_email){
         Long projectDTO = projectService.selectByMemberSeqS(member_email);
         return projectDTO;
     }
-
-
+    //http://127.0.0.1:8000/rest_jg/searchMemberSeq/abcd1234@gmail.com 호출 성공
+    @PostMapping("insert")
+    public void insert(@RequestBody ProjectDTO projectDTO){
+        projectService.insertS(projectDTO);
+    }
+    /*at Talend API Tester
+      method : post
+      http://127.0.0.1:8000/rest_jg/insert
+      {"project_title":"TESTProject","project_content":"TEST Project Content","project_status":"0","project_isdelete":"0","member_seq":1}
+      Response 200 코드 확인 완료
+    */
+    @PutMapping("updateStatus")
+    public void updateStatus(@RequestBody ProjectDTO projectDTO){
+        projectService.updateStatus(projectDTO);
+    }
+    /*at Talend API Tester
+      method : put
+      http://127.0.0.1:8000/rest_jg/updateStatus
+      {"project_status":"1","project_seq":28}
+      Response 200 코드 확인 완료
+    */
+    @PutMapping("updateIsdelete")
+    public void updateIsdelete(@RequestBody ProjectDTO projectDTO){
+        projectService.updateIsdelete(projectDTO);
+    }
+    /*at Talend API Tester
+      method : put
+      http://127.0.0.1:8000/rest_jg/updateIsdelete
+      {"project_isdelete":"1","project_seq":28}
+      Response 200 코드 확인 완료
+    */
+    @PutMapping("updateTitle")
+    public void updateTitle(@RequestBody ProjectDTO projectDTO){
+        projectService.updateTitle(projectDTO);
+    }
+    /*at Talend API Tester
+     method : put
+     http://127.0.0.1:8000/rest_jg/updateTitle
+     {"project_title":"updateTitle TEST","project_seq":28}
+     Response 200 코드 확인 완료
+   */
+    @PutMapping("updateContent")
+    public void updateContent(@RequestBody ProjectDTO projectDTO){
+        projectService.updateContent(projectDTO);
+    }
+    /*at Talend API Tester
+     method : put
+     http://127.0.0.1:8000/rest_jg/updateContent
+     {"project_content":"updateContent TEST","project_seq":28}
+     Response 200 코드 확인 완료
+   */
+    @PutMapping("updateProject")
+    public void updateProject(@RequestBody ProjectDTO projectDTO){
+        projectService.updateProject(projectDTO);
+    }
 
 }
