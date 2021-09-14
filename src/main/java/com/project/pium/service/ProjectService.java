1cc4372808c569c81221bb29a793efbb1bf3a215
JeongGu <kjg3595@naver.com>
2021-09-14 19:46:07 +0900

    정구꺼 xml 제외하고 나머지 파일 백업

--- src/main/java/com/project/pium/service/ProjectService.java
+++ src/main/java/com/project/pium/service/ProjectService.java
@@ -8,4 +8,9 @@ public interface ProjectService {
     List<ProjectDTO> listS();
     Long selectByMemberSeqS(String member_email);
     void insertS(ProjectDTO projectDTO);
+    void updateStatus(ProjectDTO projectDTO);
+    void updateIsdelete(ProjectDTO projectDTO);
+    void updateTitle(ProjectDTO projectDTO);
+    void updateContent(ProjectDTO projectDTO);
+    void updateProject(ProjectDTO projectDTO);
 }
