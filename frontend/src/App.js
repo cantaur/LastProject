65961b7d6c35b51a48e195d1b9b04bcc1ec151da
soon <sudosoon@gmail.com>
2021-09-15 15:01:38 +0900

    리액트앱 초기세팅

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,26 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import React, {useState, useEffect} from 'react';
+
 
 function App() {
+  const pubimg = process.env.PUBLIC_URL + '/img/'; 
+  const [message, setMessage] = useState("");
+  useEffect(() => {
+    fetch('/api/hello')
+    .then(response => response.text())
+    .then(message => {
+      setMessage(message);
+    });
+    
+  },[])
+  console.log(process.env.PUBLIC_URL)
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={pubimg+'logo.png'} alt="" />
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -15,7 +30,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          {message}
         </a>
       </header>
     </div>
