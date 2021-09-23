3792322c413b801534015245c93835eb1f8d124f
JeongGu <kjg3595@naver.com>
2021-09-23 21:57:04 +0900

    정구 Test2에서 풀캘린더 작업중!!

--- frontend/src/comp/Test2.js
+++ frontend/src/comp/Test2.js
@@ -6,27 +6,28 @@ import {FloatingLabel, Form, Button, Dropdown, Alert, Modal} from 'react-bootstr
 import { Link, useParams, withRouter, useHistory } from "react-router-dom";
 import {CSSTransition} from 'react-transition-group';
 import {connect} from 'react-redux';
+import FullCalendar from '@fullcalendar/react' // must go before plugins
+import dayGridPlugin from '@fullcalendar/daygrid' // a plugin!
+
 
 const pColorB = {
   backgroundColor: '#038D7F',
 }
 
 const pColor = {
-  color: '#038D7F',
+color: '#038D7F',
 }
 
 function Test2(p){
 
   return(
-    <div className="viewOutWrap">
+    <div className="viewOutWrap" style={{overflow:"scroll"}}>
       <Header/>
       <div style={{padding:"80px"}}>
-        <p>컴포넌트 경로 : src/comp/Test2.js</p>
-        <hr/>
-        <p>모듈 추가시 꼭 --save를 붙혀주세요.</p>
-        <p>예시: npm install react-redux --save (yarn 사용시 yarn add react-redux --save)</p>
-        <p>페이지 추가시 App.js에서 Test 컴포넌트 있는 부분 보시고 추가하시면 됩니당 잘 안되면 문의</p>
-        <p>그 외에는 마음대로 작업하세용 다 지워도댐 ㄱㄱㄱ</p>
+        <FullCalendar
+            plugins={[ dayGridPlugin ]}
+            initialView="dayGridMonth"
+        />
       </div>
       
       <Side/>
