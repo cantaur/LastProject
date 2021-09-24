bacd074fbc613d5e68d5810e77aea63d01015110
kaeun <attackmood@gmail.com>
2021-09-24 22:36:04 +0900

    파일 관련 롤백

--- src/main/java/com/project/pium/PiumApplication.java
+++ src/main/java/com/project/pium/PiumApplication.java
@@ -1,14 +1,12 @@
 package com.project.pium;
 
-import com.project.pium.file.FileStorageProperties;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 
-@EnableConfigurationProperties({
-		FileStorageProperties.class
-})
+
 @SpringBootApplication
 public class PiumApplication {
 
