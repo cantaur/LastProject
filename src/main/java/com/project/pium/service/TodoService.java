775aef59ee674a83b27fbf6d1b4a49ed638bc699
Park SoYeon <psy0534@gmail.com>
2021-09-29 14:36:32 +0900

    소연 Todo 작업중..

--- src/main/java/com/project/pium/service/TodoService.java
+++ src/main/java/com/project/pium/service/TodoService.java
@@ -10,22 +10,19 @@ public interface TodoService {
     List<TodoDTO>selectBySeqS(long seq);
     List<TodoDTO>progressBySeqS(long seq);
     List<TodoDTO>doneBySeqS(long seq);
-
     Long countTodoStatusS(long seq);
     Long countProgressStatusS(long seq);
     Long countDoneStatusS(long seq);
-
     List<String> showTaskS(long seq);
-
+    List<TaskDTO> showTaskNSeqS(long seq);
     List<TodoDTO>selectNoteToDoS(); // 메모 상태 10
     List<TodoDTO>selectNoteProgS(); // 메모 상태 20
     List<TodoDTO>selectNoteDoneS(); // 메모 상태 30
-    void insertNoteS(TodoDTO todo); // 메모 생성
     void updateNoteS(TodoDTO todd); // 메모 수정
-    void updateNoteStatusS(TodoDTO todo); // 메모 상태이동
     void deleteNoteS(long todo_seq); // 메모 삭제
 
-//    List<TodoDTO>selectAssignS(); // 배정된 업무 조회
-//    List<TodoDTO>selectWorkS(); // 진행중인 업무
-//    List<TodoDTO>selectDoneS(); // 완료된 업무
+    void insertNoteS(TodoDTO todo); // 메모 생성
+    void updateNoteStatusS(TodoDTO todo); // 메모 상태이동
+
+
 }
--- src/main/java/com/project/pium/service/TodoServiceImpl.java
+++ src/main/java/com/project/pium/service/TodoServiceImpl.java
@@ -25,15 +25,12 @@ public class TodoServiceImpl implements TodoService {
     public List<TodoDTO>doneBySeqS(long seq){
         return mapper.doneBySeq(seq);
     }
-
     @Override
     public Long countDoneStatusS(long seq) {
         return mapper.countDoneStatus(seq);
     }
     @Override
-    public Long countTodoStatusS(long seq){
-        return mapper.countTodoStatus(seq);
-    }
+    public Long countTodoStatusS(long seq){return mapper.countTodoStatus(seq);}
     @Override
     public Long countProgressStatusS(long seq){
         return mapper.countProgressStatus(seq);
@@ -42,38 +39,34 @@ public class TodoServiceImpl implements TodoService {
     public List<String> showTaskS(long seq){
         return mapper.showTask(seq);
     }
-
+    @Override
+    public List<TaskDTO> showTaskNSeqS(long seq){return mapper.showTaskNSeq(seq);}
     @Override
     public List<TodoDTO> selectNoteToDoS() {
         return mapper.selectNoteToDo();
     }
-
     @Override
     public List<TodoDTO> selectNoteProgS() {
         return mapper.selectNoteProg();
     }
-
     @Override
     public List<TodoDTO> selectNoteDoneS() {
         return mapper.selectNoteDone();
+    }
+    @Override
+    public void updateNoteS(TodoDTO todo) {
+        mapper.updateNote(todo);
+    }
+    @Override
+    public void deleteNoteS(long todo_seq) {
+        mapper.deleteNote(todo_seq);
     }
 
     @Override
     public void insertNoteS(TodoDTO todo) {
         mapper.insertNote(todo);
     }
-
-    @Override
-    public void updateNoteS(TodoDTO todo) {
-        mapper.updateNote(todo);
-    }
-
     @Override
     public void updateNoteStatusS(TodoDTO todo) { mapper.updateNoteStatus(todo);
-    }
-
-    @Override
-    public void deleteNoteS(long todo_seq) {
-        mapper.deleteNote(todo_seq);
     }
 }
